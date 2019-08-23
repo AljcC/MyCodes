6ff149b403e72c05860e8bc115bf73d071e54cde
ljc_rp <>
2019-08-23 22:47:47 +0800

    Update 3507.cpp

--- 洛谷/3507.cpp
+++ 洛谷/3507.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 using namespace std;
 
-
+const int MAXN = 1000010;
+int n, a[MAXN], f[MAXN];
 
 int main() {
-
+	cin >> n;
+	for(int i=1; i<=n; i++) {
+		cin >> a[i];
+	}	
+	sort(a+1, a+n+1);
+	for(int i=1; i<=n; i++) {
+		f[i] = max(f[i-1], a[i]-f[i-1]);
+	}
+	cout << f[n];
 	return 0;
 }
