9d3f9ec0ad4d616701ad4a047e0d4fd1af8cd60a
ljc_rp <>
2019-08-23 22:15:14 +0800

    Create cdt1494.cpp

---
+++ 酷町堂/cdt1494.cpp
@@ -0,0 +1,17 @@
+/*
+d[s] = d[s-{j}]+1 | {j}∈s hw[j]=true 
+*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+const MAXN = 16;
+string s;
+int d[1<<MAXN];
+
+int main() {
+	cin >> s;
+	d[0] = 0;
+	
+	return 0;
+}
