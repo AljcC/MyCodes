8ee6898aab080b7913543301727795684c349aa5
ljc_rp <>
2019-08-25 14:41:36 +0800

    Create 2890.cpp

---
+++ 洛谷/2890.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+const int MAXN = 30;
+const int MAXM = 2010;
+struct pt {
+	char c;
+	int x, y;
+} a[MAXN];
+int n, m;
+char s[MAXM];
+
+int main() {
+	cin >> m >> n >> s;
+	//for(int i=1; i<)
+	return 0;
+}
