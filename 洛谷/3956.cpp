dd7904886621adce6920da9666e90a5851def959
ljc_rp <42795073+AljcC@users.noreply.github.com>
2019-09-26 12:20:15 +0800

    Update 3956.cpp

--- 洛谷/3956.cpp
+++ 洛谷/3956.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 using namespace std;
 
-int m, n;
-int board[110][110], f[110][110];
-int dir[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-int dir2[8][2] = {{2,0}, {-2,0}, {0,2}, {0,-2}, {1,1}, {1,-1}, {-1,-1}, {-1,1}};
+int n, m, ans=1e9;
+int map[110][110], f[110][110], vis[110][110];
+int nxt[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
-void dfs(int x, int t, int cst) {
-	if(cst>=f[x][y]) return;
+bool judge(int x, int y) {
+	if(x>=1 && y>=1 && x<=m && y<=m) {
+		return 1;
+	} return 0;
+}
+
+void dfs(int x, int y, int cst, int mo) {
+	if(x<1||y<1||x>m||y>m)
+		return; 
+	if(cst >= f[x][y] ) return ;
 	f[x][y] = cst;
-	if(x==m && y==m) return;
-	for(int i=)
+
+	vis[x][y] = 1;
+
+	if(x==m && y==m) {
+        if(cst<ans)
+			ans=cst;
+        return;
+    }
+	for(int i=0; i<4; i++) {
+		int nx=x+nxt[i][0], ny=y+nxt[i][1];
+			if(map[nx][ny]) {
+				if(map[nx][ny] == map[x][y])
+					dfs(nx, ny, cst, 0);
+				else dfs(nx, ny, cst+1, 0);
+			} else if(map[nx][ny]==0){
+				if(mo==0) {
+					map[nx][ny] = map[x][y];
+					dfs(nx, ny, cst+2, 1);
+					map[nx][ny] = 0;
+				}
+			}
+	}
 }
 
 int main() {
-	memset(f, 0x3f, sizeof(f));
+	memset(f, 0x7f, sizeof(f));
 	cin >> m >> n;
 	for(int i=1; i<=n; i++) {
 		int x, y, c;
 		cin >> x >> y >> c;
-		board[x][y] = c+1;
+		map[x][y] = c+1;
 	}
-	dfs(1, 1)
+	dfs(1, 1, 0, 0);
+	if(ans != 1e9) cout << ans;
+	else cout << -1;
 	return 0;
-}
+} 	
